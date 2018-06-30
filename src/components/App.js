5fa39c56006cea55e1f80585c6c21bc09557c60a
Sheila HK <38942403+tigershk@users.noreply.github.com>
2018-06-30 17:09:10 +0100

    orders available on server

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import Results from "./Results";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
+    this.state = { menu: [] }
+
+    this.getmenu = this.getmenu.bind(this)
   }
 
-  render(){
+
+  getmenu() {
+    fetch('/menu')
+      .then(response => response.ok ? response.json() : Promise.reject(response))
+      .then(json => {
+        this.setState({
+          menu: Object.values(json)
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  componentDidMount() {
+    this.getmenu()
+  }
+
+  render() {
+    // console.log(this.state.menu)
+
     return (
-      <div>
-        Delivereat app
+      <div className="App">
+        <div className="header">
+          <h1>🍕 Pizza Right Now</h1>
+        </div>
+
+        <Results menu={this.state.menu} />
+
+
       </div>
     )
   }
