139eaceaff58a8fb7d12de709a606ff593b2ec11
Sheila HK <38942403+tigershk@users.noreply.github.com>
2018-07-07 18:55:27 +0100

    SMS working but not displayed on Admin screen yet

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -8,7 +8,8 @@ class Admin extends React.Component {
     super();
 
     this.state = {
-      orderStatus: ''
+      orderStatus: '',
+      orderMobile: 0
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -16,18 +17,22 @@ class Admin extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({ orderStatus: event.target.value }, () => this.checkStatus());
+
+    const orderName = event.target.name
+
+    this.setState({ orderStatus: event.target.value }, () => this.checkStatus(orderName));
   }
 
-  checkStatus() {
+  checkStatus(currentOrder) {
+    const custMobile = this.props.orderHistory[currentOrder].mobile;
+    const custDetails = Object.assign({}, { ["mobile"]: custMobile });
 
     if (this.state.orderStatus === "In Transit") {
-      //send sms
-
+      //retrieve sms from orderhistory and populate orderMobile
       fetch('/sendsms', {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ contact: +447762071057 })
+        body: JSON.stringify({ custDetails })
       })
         ;
       //   console.log("sms should be sent now");
@@ -36,48 +41,65 @@ class Admin extends React.Component {
 
   }
   render() {
-
+    // let myVar = "banana banana";
     return (
 
       <div className="admin">
+        <table className="admin__table">
+          <thead>
+            <tr>
+              <th className="admin__table--order"> Order Number</th>
+              <th className="admin__table--quantity"> Quantity </th>
+              <th className="admin__table--item"> Item</th>
+              <th className="admin__table--contact"> Contact </th>
+              <th className="admin__table--status"> Status</th>
+            </tr>
+          </thead>
+        </table>
+
+
         {/* Copies array of keys, reverses and then loops through to get reverse chronological order */}
         {Object.keys(this.props.orderHistory).slice().reverse().map(orderNum => {
 
           return (
             <table className="admin__table">
-              <tr>
-                <th className="admin__table--order"> Order Number</th>
-                <th className="admin__table--quantity"> Quantity </th>
-                <th className="admin__table--item"> Item</th>
-                <th className="admin__table--contact"> Contact </th>
-                <th className="admin__table--status"> Status</th>
-              </tr>
-              <tr>
-                <td> {orderNum}</td>
-                <td></td>
-                <td></td>
+              <tbody>
+                <tr>
+                  <th className="admin__table--order"> </th>
+                  <th className="admin__table--quantity">  </th>
+                  <th className="admin__table--item"> </th>
+                  <th className="admin__table--contact">  </th>
+                  <th className="admin__table--status"> </th>
+                </tr>
 
-                <td> 0776 207 1057 </td>
-                <td> <select className="status__menu" value={this.state.orderStatus} onChange={this.handleChange} >
-                  <option value="Received">Received</option>
-                  <option value="In Progress">In Progress</option>
-                  <option value="In Transit">In Transit</option>
-                  <option value="Delivered">Delivered</option>
-                </select>
-                </td>
-              </tr>
-              {Object.keys(this.props.orderHistory[orderNum]).map(lineItem => {
-                return (
-                  <tr>
-                    <td></td>
-                    <td> {this.props.orderHistory[orderNum][lineItem]} </td>
-                    <td> {lineItem}</td>
-                    <td></td>
-                    <td></td>
+                <tr key={orderNum}>
+                  <td> {orderNum}</td>
+                  <td></td>
+                  <td></td>
 
-                  </tr>
-                )
-              })}
+                  <td> 0776 207 1057 </td>
+                  <td> <select className="status__menu" name={orderNum} value={this.state.orderStatus} onChange={this.handleChange} >
+                    <option value="Received">Received</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="In Transit">In Transit</option>
+                    <option value="Delivered">Delivered</option>
+                  </select>
+                  </td>
+                </tr>
+                {Object.keys(this.props.orderHistory[orderNum]).map(lineItem => {
+                  if (lineItem === "mobile") return;
+                  return (
+                    <tr>
+                      <td></td>
+                      <td> {this.props.orderHistory[orderNum][lineItem]} </td>
+                      <td> {lineItem}</td>
+                      <td></td>
+                      <td></td>
+
+                    </tr>
+                  )
+                })}
+              </tbody>
             </table>
           )
         })}
